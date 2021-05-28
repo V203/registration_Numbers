c21f5dc27460093b28601221061080a475321688
v203 <vuyisamr203@gmail.com>
2021-05-28 16:37:32 +0200

    16:37 28/05

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+// alert("we good")
+
+
+const buttonAdd  = document.querySelector(".buttonAdd");
+const input_ = document.querySelector(".inputField");
+const show = document.querySelector(".show");
+
+buttonAdd.addEventListener("click",function(){
+    alert(input_.value)
+    
+})
+
+
+
