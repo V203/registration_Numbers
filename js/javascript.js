9a1ff96934710ede15fb66325883ea8dc93e5576
v203 <vuyisamr203@gmail.com>
2021-05-31 17:09:50 +0200

    dc

--- js/javascript.js
+++ js/javascript.js
@@ -4,9 +4,10 @@
 const buttonAdd  = document.querySelector(".buttonAdd");
 const input_ = document.querySelector(".inputField");
 const show = document.querySelector(".show");
+const selection = document.querySelector(".selection")
 
 buttonAdd.addEventListener("click",function(){
-    alert(input_.value)
+    alert(selection.value)
     
 })
 
