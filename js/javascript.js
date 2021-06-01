20b5e96e5abc0c28d061b34720ab6520a6564d95
V203 <vuyisamr203@gmail.com>
2021-06-01 20:10:02 +0200

    manjaro

--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,29 @@
 // alert("we good")
 
 
-const buttonAdd  = document.querySelector(".buttonAdd");
-const buttonShowAll = document.querySelector(".buttonShowAll");
-const buttonClear = document.querySelector(".buttonClear")
-const input_ = document.querySelector(".inputField");
-const show = document.querySelector(".show");
-const selection = document.querySelector(".selection")
-const thePlates = document.querySelector(".thePlates")
-const ca_out = document.querySelector(".ca_out")
-const cw_out = document.querySelector(".cw_out")
-const cy_out = document.querySelector(".cy_out")
-const error_out = document.querySelector(".error_out")
+var buttonAdd  = document.querySelector(".buttonAdd");
+var buttonShowAll = document.querySelector(".buttonShowAll");
+var buttonClear = document.querySelector(".buttonClear")
+var input_ = document.querySelector(".inputField");
+var show = document.querySelector(".show");
+var selection = document.querySelector(".selection")
+var thePlates = document.querySelector(".thePlates")
+var ca_out = document.querySelector(".ca_out")
+var cw_out = document.querySelector(".cw_out")
+var cy_out = document.querySelector(".cy_out")
+var error_out = document.querySelector(".error_out")
 
 var RgNm_ = RgNm()
 
 buttonAdd.addEventListener("click",function(){
-    RgNm_.setPlates(input_.value)
+    // RgNm_.setPlates(input_.value)
     // error_out.innerHTML= 
-    RgNm_.setPlates(input_.value) !== undefined ? error_out.innerHTML = RgNm_.setPlates(input_.value):"";
+    RgNm_.setPlates(input_.value) !== undefined ? setTimeout(()=>{error_out.innerHTML =""},3000)+" "+(error_out.innerHTML = RgNm_.setPlates(input_.value)) :"";
      
-    cw_out.innerHTML = RgNm_.getCW()
-    ca_out.innerHTML = RgNm_.getCA()
-    cy_out.innerHTML = RgNm_.getCY()
-    
+    var newDiv =document.createElement("div",{is:"thePlates"})
+     var newText = document.createTextNode(RgNm_.getCW())
+     newDiv.appendChild(newText)
+    document.body.insertBefore(newDiv,error_out)
     
 })
 
